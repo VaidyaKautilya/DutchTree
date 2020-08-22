3284a4dce2e2d01db47f82e8380c7f3192f56bf2
sunny <kvaidya221@gmail.com>
2020-08-22 10:33:37 +0530

    added migration and database work to the project.

---
+++ Data/DutchContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTree.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DutchTree.Data
+{
+    public class DutchContext : DbContext
+    {
+        public DutchContext(DbContextOptions<DutchContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
